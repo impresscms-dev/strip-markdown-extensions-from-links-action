6b4d3435e6cff03da6c3c0eb97dbfd0153f8096e
Thorsten Hake <mail@thorsten-hake.com>
2022-11-11 21:50:11 +0100

    Fixed linting and added jest to package.json

--- src/helpers/link-replacer.test.ts
+++ src/helpers/link-replacer.test.ts
@@ -1,7 +1,6 @@
-import {describe, expect, test, afterAll} from '@jest/globals';
-import linkReplacer from "./link-replacer";
-// @ts-ignore
-import mock from "mock-fs";
+import {describe, expect, test, afterAll} from '@jest/globals'
+import linkReplacer from "./link-replacer"
+import mock from "mock-fs"
 mock({
   'test-dir': {
     'to-a-file.md' : '',
@@ -12,17 +11,17 @@ mock({
 linkReplacer.setFilesPath("test-dir")
 describe('link-replacer', () => {
   test('normal markdown file', () => {    
-    const newContent = linkReplacer.transformMarkdownLinks("[link](to-a-file.md)");
+    const newContent = linkReplacer.transformMarkdownLinks("[link](to-a-file.md)")
     expect(newContent).toBe("[link](to-a-file)")
-  });
+  })
   test('special characters', () => {
-    const newContent = linkReplacer.transformMarkdownLinks("[link](special:characters-included.md)");
+    const newContent = linkReplacer.transformMarkdownLinks("[link](special:characters-included.md)")
     expect(newContent).toBe("[link](special%3Acharacters-included)")
-  });
+  })
   test('markdown file with fragment', () => {
-    const newContent = linkReplacer.transformMarkdownLinks("[link](to-a-file.md#my-headline)");
+    const newContent = linkReplacer.transformMarkdownLinks("[link](to-a-file.md#my-headline)")
     expect(newContent).toBe("[link](to-a-file#my-headline)")
-  });
+  })
   // Does not work due to used transform-markdown-links dependency
   // test('check that link references work', () => {
   //   const newContent = linkReplacer.transformMarkdownLinks(`[link][some-reference]
@@ -34,5 +33,5 @@ describe('link-replacer', () => {
   // });
   afterAll(() => {
     mock.restore()    
-  });
-});
+  })
+})
