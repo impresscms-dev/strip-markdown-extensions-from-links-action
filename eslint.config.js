352ac8830c21f98878d33d8f926d1ca0cd45566f
Raimondas Rimkevičius (aka MekDrop) <github@mekdrop.name>
2025-04-09 03:01:53 +0300

    Bump eslint to ^v9

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import jestPlugin from 'eslint-plugin-jest'
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...jestPlugin.environments.globals.globals
+      }
+    },
+    plugins: {
+      jest: jestPlugin
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'camelcase': 'off',
+      'func-call-spacing': ['error', 'never'],
+      'no-array-constructor': 'error',
+      'no-useless-constructor': 'error',
+      'prefer-const': 'error',
+      'semi': ['error', 'never'],
+      'no-useless-escape': 'off',
+      
+      // Jest plugin rules
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/valid-expect': 'error'
+    }
+  }
+]
